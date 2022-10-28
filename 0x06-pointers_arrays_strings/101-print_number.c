782f1b49b1db7c8d13515a162f20932eecb834d3
Assent <chasteassent@gmail.com>
2022-10-28 11:44:13 -0700

    reset_to_98.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,3 @@
-
-Advanced task 2
 #include "main.h"
 /**
  *  * print_number - prints an integer.
@@ -34,4 +32,3 @@ void print_number(int n)
 													_putchar(((m / count) % 10) + 48);
 														}
 }
-
